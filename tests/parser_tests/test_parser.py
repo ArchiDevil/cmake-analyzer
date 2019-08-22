341cdb0e5b00ff3a3f02d172faaa5d3249213faf
Denis Bezykornov <archidevil52@gmail.com>
2019-08-23 00:43:25 +0300

    Add a test for corner case that was not described in cmake-language

--- tests/parser_tests/test_parser.py
+++ tests/parser_tests/test_parser.py
@@ -21,6 +21,19 @@ def test_parser_can_parse_simplest_cmake():
         assert arg['arg'] == [args_desc[i]]
 
 
+def test_parser_can_parse_space_after_command():
+    pp = parser.Parser('core/simple_grammar.ebnf')
+    ast = pp.parse_file('tests/parser_tests/space_after_command.txt')
+    assert len(ast) == 1
+    command = ast[0]['command']
+    assert command['name'] == 'add_library'
+    assert len(command['args']) == 3
+
+    args_desc = ['main', 'STATIC', 'main.cpp']
+    for i, arg in enumerate(command['args']):
+        assert arg['arg'] == [args_desc[i]]
+
+
 def test_parser_can_parse_unquoted_quoted_cmake():
     pp = parser.Parser('core/simple_grammar.ebnf')
     ast = pp.parse_file('tests/parser_tests/unquoted_quoted.txt')
@@ -36,5 +49,6 @@ def test_parser_can_parse_unquoted_quoted_cmake():
 
 def test_parser_can_save_model():
     pp = parser.Parser('core/simple_grammar.ebnf')
-    pp.save_model('model.pyc') # .pyc to avoid this file to be commited to the repo
+    # .pyc to avoid this file to be commited to the repo
+    pp.save_model('model.pyc')
     assert os.path.exists('model.pyc')
