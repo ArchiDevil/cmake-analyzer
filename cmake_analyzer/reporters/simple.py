f1347295803cf9232a16485452f9842ab01f6d65
Denis Bezykornov <archidevil52@gmail.com>
2019-09-21 15:33:51 +0300

    Make simple reporter report diagnostic

--- cmake_analyzer/reporters/simple.py
+++ cmake_analyzer/reporters/simple.py
@@ -1,8 +1,12 @@
+from typing import List
+
+from cmake_analyzer.core.reporter_base import Diagnostic
+
 class SimpleReporter:
     @staticmethod
-    def report(iterable):
+    def report(iterable: List[Diagnostic]):
         for diag in iterable:
-            print('{}:{} - {}'.format(diag.file, diag.line, diag.message))
+            print('{}:{} - {} [{}]'.format(diag.file, diag.line, diag.message, diag.module))
 
     @staticmethod
     def end():
--- tests/reporters_tests/test_simple_reporter.py
+++ tests/reporters_tests/test_simple_reporter.py
@@ -21,25 +21,32 @@ def test_can_create_diagnostic_from_node():
     assert diag.module == module
 
 
-
 def test_simple_reporter_reports_in_console():
+    reporter = simple.SimpleReporter()
+    line = 21
+    node = {'parseinfo': Parseinfo(line)}
+    file = 'test'
+    message = 'error!'
+    module = 'test_module'
+    simple_diag = reporter_base.create_diagnostic(node, message)
+    diag = reporter_base.create_full_diagnostic(simple_diag, file, module)
+    old_print = print
+
     class Checker(object):
         result = False
 
         @staticmethod
         def printer(*args):
             Checker.result = True
+            assert len(args) == 1
+            assert file in args[0]
+            assert message in args[0]
+            assert str(line + 1) in args[0]
+            assert module in args[0]
 
-    reporter = simple.SimpleReporter()
-    node = {'parseinfo': Parseinfo(21)}
-    file = 'test'
-    message = 'error!'
-    module = 'test_module'
-    simple_diag = reporter_base.create_diagnostic(node, message)
-    diag = reporter_base.create_full_diagnostic(simple_diag, file, module)
     builtins.print = Checker.printer
-
     reporter.report([diag])
+    builtins.print = old_print
     assert Checker.result
 
 
